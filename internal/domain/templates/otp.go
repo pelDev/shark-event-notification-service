86c71d69981d9904dcbe814143c2db02c9209299
crazy_dev <pelumidevelops18@gmail.com>
2025-12-04 23:22:19 +0100

    send otp email

--- internal/domain/templates/otp.go
+++ internal/domain/templates/otp.go
@@ -1,6 +1,30 @@
 package templates
 
+import (
+	"fmt"
+)
+
 type OtpData struct {
 	OtpCode  string `json:"otp_code"`
-	ValidFor string `json:"valid_for"` // e.g. "5 minutes"
+	ValidFor int    `json:"valid_for"` // e.g. "5 minutes"
+}
+
+func (o *OtpData) isEmailTemplateData() {}
+
+func (o *OtpData) GetMessage(emailFrom, email, subject, html string) []byte {
+	message := fmt.Sprintf(
+		"From: %s\r\n"+
+			"To: %s\r\n"+
+			"Subject: %s\r\n"+
+			"MIME-Version: 1.0\r\n"+
+			"Content-Type: text/html; charset=\"UTF-8\"\r\n"+
+			"\r\n"+
+			"%s\r\n",
+		emailFrom,
+		email,
+		subject,
+		html,
+	)
+
+	return []byte(message)
 }
--- internal/domain/templates/root.go
+++ internal/domain/templates/root.go
@@ -27,6 +27,14 @@ func ParseTemplateData(templateName string, data map[string]interface{}, out *Em
 		*out = &result
 		return nil
 
+	case "otp":
+		var result OtpData
+		if err := json.Unmarshal(raw, &result); err != nil {
+			return fmt.Errorf("failed to unmarshal otp email data: %w", err)
+		}
+		*out = &result
+		return nil
+
 	default:
 		return fmt.Errorf("unknown template name: %s", templateName)
 	}
