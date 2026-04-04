47cdba84753e3ec2cbb390b523d194b9f62ae40f
crazy_dev <pelumidevelops18@gmail.com>
2026-04-04 11:54:21 +0100

    feat: add reminder

--- internal/domain/templates/event_reminder.go
+++ internal/domain/templates/event_reminder.go
@@ -1,10 +1,60 @@
 package templates
 
-type OrganizerEventReminderData struct {
-	EventTitle string
-	EventDate  string
-	TimeLeft   string // "24 hours", "6 hours", "1 hour"
-	EventID    string
+import "fmt"
+
+type ReminderVenue struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
 }
 
-// TODO: Implement EmailTemplateData interface methods
+type ReminderEvent struct {
+	Name            string        `json:"name"`
+	Slug            string        `json:"slug"`
+	Status          string        `json:"status"`
+	Type            string        `json:"type"`
+	AccessType      string        `json:"access_type"`
+	RequiresPayment bool          `json:"requires_payment"`
+	MinTicketPrice  string        `json:"min_ticket_price"`
+	Venue           ReminderVenue `json:"venue"`
+}
+
+type ReminderOccurrence struct {
+	Label     *string `json:"label,omitempty"`
+	StartDate string  `json:"start_date"`
+	StartTime string  `json:"start_time"`
+	EndTime   string  `json:"end_time"`
+	Duration  string  `json:"duration"`
+	Capacity  *int    `json:"capacity,omitempty"`
+}
+
+type ReminderEmailData struct {
+	Subject    string             `json:"subject"`
+	CustomNote string             `json:"custom_note"`
+	Event      ReminderEvent      `json:"event"`
+	Occurrence ReminderOccurrence `json:"occurrence"`
+}
+
+func (e *ReminderEmailData) isEmailTemplateData() {}
+
+func (e *ReminderEmailData) GetMessage(emailFrom, email, subject, html string) []byte {
+	message := fmt.Sprintf(
+		"From: %s\r\n"+
+			"To: %s\r\n"+
+			"Subject: %s\r\n"+
+			"MIME-Version: 1.0\r\n"+
+			"Content-Type: text/html; charset=\"UTF-8\"\r\n"+
+			"\r\n"+
+			"%s\r\n",
+		emailFrom,
+		email,
+		subject,
+		html,
+	)
+
+	return []byte(message)
+}
+
+func (e *ReminderEmailData) GetPreHeader() *string {
+	preHeader := "Reminder: Your event is coming up soon. Don't miss it!"
+	return &preHeader
+}
--- internal/domain/templates/root.go
+++ internal/domain/templates/root.go
@@ -108,6 +108,14 @@ func ParseTemplateData(templateName string, data map[string]interface{}, out *Em
 		*out = &result
 		return nil
 
+	case "event-reminder":
+		var result ReminderEmailData
+		if err := json.Unmarshal(raw, &result); err != nil {
+			return fmt.Errorf("failed to unmarshal reminder email data: %w", err)
+		}
+		*out = &result
+		return nil
+
 	default:
 		return fmt.Errorf("unknown template name: %s", templateName)
 	}
